332ed77a7274f5b4609350f5a16e3e4e6018fcd3
PMA <pierre-marie.allard@unifr.ch>
2024-01-29 10:53:30 +0100

    Name stds and formats

--- met_annot_unifier/aligner/parser.py
+++ met_annot_unifier/aligner/parser.py
@@ -20,3 +20,25 @@ def parse_isdb(file_path):
     data = pd.read_csv(file_path, sep="\t")
     # Extract necessary columns and any other processing
     return data
+
+
+def standardize_column_names(df, original_column_name, final_column_name):
+    """
+    Standardizes the column names of a dataframe.
+
+    Args:
+    df (pandas.DataFrame): The dataframe to standardize.
+    original_column_name (str): The current name of the column to be standardized.
+    final_column_name (str): The final standardized name for the column.
+
+    Returns:
+    pandas.DataFrame: The dataframe with standardized column names.
+
+    Example:
+    >>> df = pd.DataFrame({'InChIKey-Planar': [1, 2], 'OtherColumn': [3, 4]})
+    >>> standardized_df = standardize_column_names(df, 'InChIKey-Planar', 'InChiKey')
+    >>> standardized_df.columns
+    Index(['InChiKey', 'OtherColumn'], dtype='object')
+    """
+    standardized_df = df.rename(columns={original_column_name: final_column_name})
+    return standardized_df
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,4 +1,6 @@
-from met_annot_unifier.aligner.parser import parse_gnps, parse_isdb, parse_sirius
+import pandas as pd
+
+from met_annot_unifier.aligner.parser import parse_gnps, parse_isdb, parse_sirius, standardize_column_names
 
 # Path to your sample data files
 GNPS_SAMPLE_PATH = "tests/data/gnps_output_example.tsv"
@@ -37,3 +39,23 @@ def test_parse_isdb():
     assert "feature_id" in isdb_data.columns, "feature_id column is missing"
     assert "short_inchikey" in isdb_data.columns, "IK2D column is missing"
     # ... add more assertions as needed ...
+
+
+def test_successful_column_renaming():
+    df = pd.DataFrame({"InChIKey-Planar": [1, 2], "OtherColumn": [3, 4]})
+    result = standardize_column_names(df, "InChIKey-Planar", "InChiKey")
+    assert "InChiKey" in result.columns
+    assert "InChIKey-Planar" not in result.columns
+
+
+def test_no_change_when_column_missing():
+    df = pd.DataFrame({"SomeOtherColumn": [1, 2], "OtherColumn": [3, 4]})
+    result = standardize_column_names(df, "InChIKey-Planar", "InChiKey")
+    assert "InChiKey" not in result.columns
+    assert "InChIKey-Planar" not in result.columns
+
+
+def test_preservation_of_other_data():
+    df = pd.DataFrame({"InChIKey-Planar": [1, 2], "OtherColumn": [3, 4]})
+    result = standardize_column_names(df, "InChIKey-Planar", "InChiKey")
+    assert all(result["OtherColumn"] == df["OtherColumn"])
